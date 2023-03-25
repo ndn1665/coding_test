6887cfbcafa1d1fc8a64dbacc3d9e6cfcf412736
ndn1665 <ndn1665@daum.net>
2023-03-25 21:45:36 +0900

    the book's answer

--- examples/binary_search/tteokbokki.py
+++ examples/binary_search/tteokbokki.py
@@ -1,30 +1,19 @@
 count, inquire = map(int, input().split())#동빈이의 떡 갯수, 손님이 원하는 최소 떡의 길이
 array = list(map(int, input().split()))#동빈이의 떡 갯수만큼 각 떡의 길이 입력
-array.sort()
 
+start = 0
+end = max(array)
 
-def binary_search(inquire,array,start,end):
-    result = cut(array)#제일 처음 상황에서 잘라주기
-    if result == inquire:
-        return array[0]
-
+result = 0
+while(start <= end):
+    total = 0
     mid = (start + end)//2
-
-    array = array[mid:]
-
-    if result > inquire:
-        start = mid
-        return binary_search(inquire,array, start, end)
-    if result < inquire:
-        start = (mid +start)//2
-        return binary_search(inquire, array, start, end)
-
-
-def cut(array):
-    cutter = array[0]
-    result = 0
     for i in array:
-        result += (i - cutter)
-    return result
-
-print(binary_search(inquire,array,0,count-1))
+        if i >= mid:
+            total += i-mid
+    if total < inquire:
+        end = mid -1
+    else:
+        result = mid
+        start = mid + 1
+print(result)
